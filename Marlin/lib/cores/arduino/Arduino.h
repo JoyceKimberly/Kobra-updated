89dc71fb3cec3c896eac62879bdcf6ac85bd719d
Jo Kim <JoyceKimberly@users.noreply.github.com>
2023-05-20 14:28:31 +0200

    Misc updates

--- Marlin/lib/cores/arduino/Arduino.h
+++ Marlin/lib/cores/arduino/Arduino.h
@@ -23,19 +23,20 @@
 #include "wirish.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"{
 #endif // __cplusplus
 // Weak empty variant initialization function.
 // May be redefined by variant files.
 extern void initVariant() __attribute__((weak));
 
+void yield( void ) ;
+
 extern void setup(void) ;
 extern void loop(void) ;
 
-void yield(void);
 #ifdef __cplusplus
 } // extern "C"
-#endif // __cplusplus
+#endif
 
 // Include pins variant
 #include "pins_arduino.h"
--- Marlin/lib/cores/arduino/HardwareSerial.h
+++ Marlin/lib/cores/arduino/HardwareSerial.h
@@ -1,37 +1,23 @@
-/******************************************************************************
- * The MIT License
- *
- * Copyright (c) 2010 Perry Hung.
- * Copyright (c) 2011, 2012 LeafLabs, LLC.
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *****************************************************************************/
+/*
+  Copyright (c) 2015 Arduino LLC.  All right reserved.
 
-/**
- * @file wirish/include/wirish/HardwareSerial.h
- * @brief Wirish serial port interface.
- */
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
-#ifndef _WIRISH_HARDWARESERIAL_H_
-#define _WIRISH_HARDWARESERIAL_H_
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef HardwareSerial_h
+#define HardwareSerial_h
 
 #include <inttypes.h>
 
@@ -114,12 +100,13 @@ struct usart_dev;
  * Also added new functions for peek() and availableForWrite()
  * Note. AvailableForWrite is only a stub function in the cpp
  */
-class HardwareSerial : public Stream {
 
+class HardwareSerial : public Stream
+{
 public:
-    HardwareSerial(struct usart_dev *usart_device,
-                   uint8_t tx_pin,
-                   uint8_t rx_pin);
+  HardwareSerial(struct usart_dev *usart_device,
+                 uint8_t tx_pin,
+                 uint8_t rx_pin);
 
     HardwareSerial(M4_USART_TypeDef *base);
     unsigned char _rx_buffer[SERIAL_RX_BUFFER_SIZE];
@@ -143,12 +130,12 @@ public:
     /* Pin accessors */
     int txPin(void) { return this->tx_pin; }
     int rxPin(void) { return this->rx_pin; }
-	
-	operator bool() { return true; }
 
-    /* Escape hatch into libmaple */
-    /* FIXME [0.0.13] documentation */
-    struct usart_dev* c_dev(void) { return this->usart_device; }
+  operator bool() { return true; }
+
+  /* Escape hatch into libmaple */
+  /* FIXME [0.0.13] documentation */
+  struct usart_dev *c_dev(void) { return this->usart_device; }
 
     bool connected() {};
     void flushTX() { flush(); };
@@ -157,10 +144,12 @@ public:
     // Interrupt handlers - Not intended to be called externally
     void _rx_complete_callback(unsigned char c);
     void set_buffer_head(rx_buffer_index_t index);
+
 private:
-    struct usart_dev *usart_device;
-    uint8_t tx_pin;
-    uint8_t rx_pin;
+  struct usart_dev *usart_device;
+  uint8_t tx_pin;
+  uint8_t rx_pin;
+
   protected:
 	M4_USART_TypeDef *uart_base;
     // Has any byte been written to the UART since begin()
@@ -177,4 +166,4 @@ private:
 #endif
 };
 
-#endif	//_WIRISH_HARDWARESERIAL_H_
+#endif
