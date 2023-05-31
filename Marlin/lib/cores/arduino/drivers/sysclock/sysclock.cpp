4ea7271b62018679c238eb41689627e2e3fc6c21
Jo Kim <JoyceKimberly@users.noreply.github.com>
2023-05-31 23:37:39 +0200

    Update sysclock.cpp

--- Marlin/lib/cores/arduino/drivers/sysclock/sysclock.cpp
+++ Marlin/lib/cores/arduino/drivers/sysclock/sysclock.cpp
@@ -6,42 +6,48 @@ system_clock_frequencies_t SYSTEM_CLOCK_FREQUENCIES = {0};
 //__attribute__((weak))
 void sysclock_init(void)
 {
+    en_clk_sys_source_t  enSysClkSrc;
+    MEM_ZERO_STRUCT(enSysClkSrc);
+
     // setup divisors for the different bus clocks
-    stc_clk_sysclk_cfg_t sysClkConf = {
-        .enHclkDiv  = ClkSysclkDiv1,  // 168MHz
-        .enExclkDiv = ClkSysclkDiv2,  // 84MHz
-        .enPclk0Div = ClkSysclkDiv1,  // 168MHz
-        .enPclk1Div = ClkSysclkDiv2,  // 84MHz
-        .enPclk2Div = ClkSysclkDiv4,  // 42MHz
-        .enPclk3Div = ClkSysclkDiv4,  // 42MHz
-        .enPclk4Div = ClkSysclkDiv1,  // 84MHz
-    };
+    stc_clk_sysclk_cfg_t sysClkConf;
+    MEM_ZERO_STRUCT(sysClkConf);
+    sysClkConf.enHclkDiv  = ClkSysclkDiv1;
+    sysClkConf.enExclkDiv = ClkSysclkDiv4;
+    sysClkConf.enPclk0Div = ClkSysclkDiv1;
+    sysClkConf.enPclk1Div = ClkSysclkDiv2;
+    sysClkConf.enPclk2Div = ClkSysclkDiv4;
+    sysClkConf.enPclk3Div = ClkSysclkDiv4;
+    sysClkConf.enPclk4Div = ClkSysclkDiv16;
     CLK_SysClkConfig(&sysClkConf);
 
+    CLK_SetPeriClkSource(ClkPeriSrcPclk);
+
     // configure and enable XTAL clock source
-    stc_clk_xtal_cfg_t xtalConf = {
-        .enFastStartup = Enable,
-        .enMode = ClkXtalModeOsc,
-        .enDrv = ClkXtalLowDrv,
-    };
+    stc_clk_xtal_cfg_t   xtalConf;
+    MEM_ZERO_STRUCT(xtalConf);
+    xtalConf.enFastStartup = Enable;
+    xtalConf.enMode = ClkXtalModeOsc;
+    xtalConf.enDrv = ClkXtalLowDrv;
     CLK_XtalConfig(&xtalConf);
     CLK_XtalCmd(Enable);
 
     // configure PLL using XTAL clock as source
-    stc_clk_mpll_cfg_t pllConf = {
-        .PllpDiv = 16u,
-        .PllqDiv = 16u,
-        .PllrDiv = 16u,
-        .plln = 30u,
-        .pllmDiv = 1u,
-    };
+    stc_clk_mpll_cfg_t   pllConf;
+    MEM_ZERO_STRUCT(pllConf);
+    pllConf.PllpDiv = 2u; /* MLLP = 168M */
+    pllConf.PllqDiv = 2u; /* MLLQ = 168M */
+    pllConf.PllrDiv = 2u; /* MLLR = 168M */
+    pllConf.plln = 42u;   /* 8M*42 = 336M */
+    pllConf.pllmDiv = 1u; /* XTAL 8M / 1 */
     CLK_SetPllSource(ClkPllSrcXTAL);
     CLK_MpllConfig(&pllConf);
 
     // enable MPLL and wait until ready
     CLK_MpllCmd(Enable);
-    while (CLK_GetFlagStatus(ClkFlagMPLLRdy) != Set)
-        ;
+    while (Set != CLK_GetFlagStatus(ClkFlagMPLLRdy))
+    {
+    }
 
     // switch the system clock to MPLL
     CLK_SetSysClkSource(CLKSysSrcMPLL);
